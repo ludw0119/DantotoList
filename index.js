deb3c5669ea6d31897fb0702243aaf57c642e833
ludw0119 <ludw0119@stud.kea.dk>
2019-05-25 12:14:43 +0200

    deleting from database without refreshing page works

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*---------------------------------------------------------------displaying-----------------------------------------------------------------*/
 function get() {
   fetch("https://dantoto-eb44.restdb.io/rest/dantoto-users", {
     method: "get",
@@ -14,8 +15,6 @@ function get() {
     });
 }
 
-get();
-
 function showInput(user) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
@@ -25,5 +24,46 @@ function showInput(user) {
   clone.querySelector("#wins").textContent = user.Wins;
   clone.querySelector("#looses").textContent = user.Looses;
   clone.querySelector("#rating").textContent = user.Rating;
+  clone.querySelector(".removeButton").id = "removeButton" + user._id;
+
+  clone.querySelector(".removeButton").addEventListener("click", e => {
+    e.target.parentElement.parentElement.remove();
+    deleteTask(user._id); //wywołanie funkcji "detete task"
+    //console.log(task._id);
+  });
+
   document.querySelector("table").appendChild(clone);
 }
+
+/*---------------------------------------------------------------deleting-----------------------------------------------------------------*/
+
+/*document.querySelector("table").addEventListener("click", clickList);
+
+function clickList(event) {
+  if (event.target.classList.contains("removeButton")) {
+    //event.target.parentElement.remove();
+    let removeButtonId = event.target.id;
+    let toBeRemoved = removeButtonId.substring(12);
+    //console.log(toBeRemoved);
+    deleteTask(toBeRemoved);
+  }
+}*/
+
+function deleteTask(id) {
+  fetch("https://dantoto-eb44.restdb.io/rest/dantoto-users/" + id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5ce6c77b780a473c8df5cb6d",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
+// Figures out if a remove button was clicked
+// If so, calls clickRemove
+get();
